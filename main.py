75d4a9ca10d7eb858621e6679a73fb41d0f71d57
Rahul NS <rahul9380767545@gmail.com>
2024-12-05 23:29:45 +0530

    add score board

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ snake_body = [] #multiple tile objects
 velocity_x = 0
 velocity_y = 0
 game_over = False
+score = 0
 
 def change_direction(e):
     global velocity_x, velocity_y
@@ -60,7 +61,7 @@ def change_direction(e):
         velocity_y = 0
 
 def move():
-    global snake, game_over, food, snake_body
+    global snake, game_over, food, snake_body, score
 
     if game_over:
         return
@@ -79,6 +80,7 @@ def move():
         snake_body.append(Tile(food.x, food.y))
         food.x = random.randint(0, COLS-1) * TILE_SIZE
         food.y = random.randint(0, ROWS-1) * TILE_SIZE
+        score += 1
 
     # update snake body
     for i in range(len(snake_body)-1, -1, -1):
@@ -95,7 +97,7 @@ def move():
     snake.y += velocity_y * TILE_SIZE
 
 def draw():
-    global snake
+    global snake, food, snake_body, game_over, score
     move()
 
     canvas.delete('all')
@@ -108,6 +110,11 @@ def draw():
 
     for tile in snake_body:
         canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill='lime green')
+
+    if game_over:
+        canvas.create_text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, font='Arial 20', text=f'Game Over: {score}', fill='white')
+    else:
+        canvas.create_text(30, 20, font='Arial 10', text=f'Score: {score}', fill='white')
 
     window.after(100, draw) #100ms = 1/10 second, 10 frames per second
 
